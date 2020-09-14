a35a6d117b06af73f6f33cc99cbaf0aa817b2344
ifeanyilucky <wfguru2017@gmail.com>
2020-09-14 11:27:16 +0100

    updates

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -16,14 +16,32 @@ const Footer = () => {
           </p>
           <div>
             <span className={style.icons}>
-              <FaInstagram />
+              <a
+                href="https://instagram.com/pmpd"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaInstagram />
+              </a>
             </span>
             <span className={style.icons}>
-              <FaTwitter />
+              <a
+                href="https://twitter.com/pmpd96396985?lang=en"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaTwitter />
+              </a>
             </span>
 
             <span className={style.icons}>
-              <FaFacebookF />
+              <a
+                href="https://www.facebook.com/Project-Manner-PD-2002274383436074/services/?service_id=2131564690224959"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaFacebookF />{" "}
+              </a>
             </span>
           </div>
         </footer>
